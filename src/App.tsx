cc62e41515921af599ea3e102672db9f169569d5
VuTan <106821592+VuTan@users.noreply.github.com>
2024-06-04 22:38:50 +0700

    delete newProduct add cardSlider

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
-import ReactDOM from "react-dom/client";
 import './App.css';
 import Header from "./component/Hearder/Header";
 import Footer from "./component/Footer/Footer";
+import Banner from "./component/Home/banner";
+import CardSlider from "./component/CardSlider";
 
 function App() {
-  return (
-      <div>
-        <Header></Header>
-        <a href={""}> vla </a>
-        <Footer></Footer>
-      </div>
+    return (
+        <div>
+            <Header></Header>
+            <Banner></Banner>
+            <CardSlider></CardSlider>
+            <Footer></Footer>
+        </div>
 
-  )
+    )
 }
 
 export default App;
