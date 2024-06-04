8729286dcdf80a74c959dcf1ca77c835dfe5a884
TuanITNe <21130235@st.hcmuaf.edu.vn>
2024-06-04 23:21:34 +0700

    update Reponsive header

--- src/component/Hearder/Header.tsx
+++ src/component/Hearder/Header.tsx
@@ -1,12 +1,23 @@
-
-import React from "react";
 import "./Header.scss";
 import search from "../images/search-icon.png";
 import heart from "../images/heart-icon.png";
 import cart from "../images/cart-icon.png";
 import logoshose from "../images/logoshose.png";
+import hamburger from "../images/hamburger.png";
+import closeIcon from "../images/close-icon.png";
+import React, { useState } from 'react';
 
-const header = () => {
+const Header = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
+    const closeMenu = () => {
+        setMenuOpen(false);
+    };
+
     return (
         <header>
             <div className="top-bar">
@@ -17,13 +28,18 @@ const header = () => {
                     <span>Cửa hàng</span>
                     <span>08am - 22pm</span>
                     <span>T2 - CN</span>
-                    <span><a href="#">Đăng nhập</a></span>
                 </div>
             </div>
-            <nav className="botton-bar">
-                <div className="botton-left">
-                    <div className="logo">
-                        <a href="#">2handtropical</a>
+            <nav>
+                <div className="logo">
+                    <a href="#">2handtropical</a>
+                </div>
+                <div className="hamburger" onClick={toggleMenu}>
+                    <img src={hamburger} alt="Menu"/>
+                </div>
+                <div className={`menu-bar ${menuOpen ? 'open' : ''}`}>
+                    <div className="close-btn" onClick={closeMenu}>
+                        <img src={closeIcon} alt="Close"/>
                     </div>
                     <ul className="menu">
                         <li><a href="#">Trang chủ</a></li>
@@ -33,15 +49,17 @@ const header = () => {
                         <li><a href="#">Bộ sưu tập</a></li>
                         <li><a href="#">Tin tức</a></li>
                     </ul>
-                </div>
-                <div className="botton-right">
-                    <div className="search">
-                        <input type="text" placeholder="Tìm kiếm"/>
-                        <button type="submit"><img src={search} alt="search"/></button>
-                    </div>
-                    <div className="icons">
-                        <a href="#"><img src={heart}/></a>
-                        <a href="#"><img src={cart}/></a>
+                    <div className="search-icons">
+                        <div className="search">
+                            <input type="text" placeholder="Tìm kiếm"/>
+                            <button type="submit"><img src={search} alt="search"/></button>
+                        </div>
+                        <div className="icons">
+                            <a href="#"><img src={heart}/></a>
+                            <a href="#"><img src={cart}/></a>
+                            <a className="login" href="#">Đăng nhập</a>
+                        </div>
+
                     </div>
                 </div>
             </nav>
@@ -49,4 +67,4 @@ const header = () => {
     )
 }
 
-export default header;
+export default Header;
