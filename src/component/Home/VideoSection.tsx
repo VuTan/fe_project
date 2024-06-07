82e22f4064ba8e94f5d9450cf92a645c06ff687f
unknown <21130219@st.hcmuaf.edu.vn>
2024-06-07 14:02:28 +0700

    thay doi lai trong VideoSection va NewCollection

--- src/component/Home/VideoSection.tsx
+++ src/component/Home/VideoSection.tsx
@@ -4,6 +4,7 @@ import './VideoSection.css';
 const VideoSection: React.FC = () => {
     const [isPlaying, setIsPlaying] = useState(false);
     const [isEnded, setIsEnded] = useState(false);
+    const [showControls, setShowControls] = useState(true);
     const videoRef = useRef<HTMLVideoElement>(null);
 
     useEffect(() => {
@@ -17,7 +18,6 @@ const VideoSection: React.FC = () => {
     }, []);
 
     const handlePlay = () => {
-
         const video = videoRef.current;
         if (video) {
             if (isPlaying) {
@@ -33,23 +33,33 @@ const VideoSection: React.FC = () => {
         }
     };
 
+    const handleMouseEnter = () => {
+        setShowControls(true);
+    };
+
+    const handleMouseLeave = () => {
+        setShowControls(false);
+    };
+
     return (
         <div className="container">
             <h2 className="section-title">2handtropcial tvc</h2>
-            <div className="video-section">
+            <div className="video-section" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
                 <video ref={videoRef} id="video" className="main-video" poster="">
                     <source src="/videos/your_video.mp4" type="video/mp4"/>
                     Your browser does not support the video tag.
                 </video>
-                <div className="play-button" onClick={handlePlay}>
-                    <svg width="100" height="100" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        {isPlaying ? (
-                            <path d="M6 19h4V5H6v14zm8-14v14h4V5h-4z" fill="white"/>
-                        ) : (
-                            <path d="M8 5V19L19 12L8 5Z" fill="white"/>
-                        )}
-                    </svg>
-                </div>
+                {showControls && (
+                    <div className={`play-button ${isPlaying ? 'hidden' : ''}`} onClick={handlePlay}>
+                        <svg width="100" height="100" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            {isPlaying ? (
+                                <path d="M6 19h4V5H6v14zm8-14v14h4V5h-4z" fill="white"/>
+                            ) : (
+                                <path d="M8 5V19L19 12L8 5Z" fill="white"/>
+                            )}
+                        </svg>
+                    </div>
+                )}
             </div>
             <div className="text-section">
                 <h1>SỰ LỰA CHỌN HÀNG ĐẦU</h1>
