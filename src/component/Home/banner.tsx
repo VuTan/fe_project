d6e17ff5966d5bb316e78df54be791ebcec7ec7d
unknown <21130219@st.hcmuaf.edu.vn>
2024-07-02 23:14:52 +0700

    creat translate

--- src/component/Home/banner.tsx
+++ src/component/Home/banner.tsx
@@ -2,24 +2,25 @@ import React from "react";
 import "./banner.scss";
 import Button from "../Button/Button";
 import Marquee from "react-fast-marquee";
+import "../../i18n/i18n"
+import {useTranslation} from 'react-i18next'
+const Banner = () => {
+    const { t } = useTranslation('banner')
 
-const banner = () => {
-    return (<div className="banner-contain">
+    return <div className="banner-contain">
         <div className="banner-top">
             <span>2handtroical</span>
-            <p>Khám phá bộ sưu tập màu hè tại. <span>@instagram.2handtropical</span></p>
+            <p>{t('ex')} <span> @instagram.2handtropical</span></p>
         </div>
         <div className="banner">
             <div className="banner-content">
                 <p className="brand"> 2handtropical </p>
-                <p className="title">CREATE THE UTOPIA</p>
-                <p className="content">Cùng 2handtropical kiến tạo nên những điều không tưởng. Mỗi bước chân ta đi
-                    đến
-                    một vùng đất mới
-                    <br/>Đều là những trải nghiệm và những bài học mới để trưởng thành hơn</p>
+                <p className="title">{t("ex1")}</p>
+                <p className="content">{t("content")}
+                    <br/>{t("content2")}</p>
                 <div className="button">
-                    <Button title="Đến của hàng" isBlack/>
-                    <Button title="Tư vấn" isBlack={false}/>
+                    <Button title={t("store")} isBlack/>
+                    <Button title={t("advise")} isBlack={false}/>
 
                 </div>
             </div>
@@ -34,7 +35,7 @@ const banner = () => {
                 </div>
             </Marquee>
         </div>
-    </div>)
+    </div>
 }
 
-export default banner;
+export default Banner;
--- src/i18n/i18n.tsx
+++ src/i18n/i18n.tsx
@@ -3,20 +3,33 @@ import { initReactI18next } from "react-i18next";
 
 import notfound_EN from "../locales/en/notfound.json"
 import header_EN from "../locales/en/header.json"
+import footer_EN from "../locales/en/footer.json"
+import banner_EN from "../locales/en/banner.json"
+import collectionvideo_EN from "../locales/en/collectionvideo.json"
+
+
 import notfound_VI from "../locales/vi/notfound.json";
 import header_VI from "../locales/vi/header.json";
+import footer_VI from "../locales/vi/footer.json";
+import banner_VI from "../locales/vi/banner.json";
+import collectionvideo_VI from "../locales/vi/collectionvideo.json";
+
 
 
 export const resources = {
     en: {
         notfound: notfound_EN,
         header: header_EN,
-
+        footer: footer_EN,
+        banner: banner_EN,
+        collectionvideo: collectionvideo_EN
     },
     vi: {
         notfound: notfound_VI,
         header: header_VI,
-
+        footer: footer_VI,
+        banner: banner_VI,
+        collectionvideo: collectionvideo_VI
     }
 }
 
