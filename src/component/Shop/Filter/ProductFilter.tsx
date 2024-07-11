ea00de1ffd6bf7b9b2202d255720a4c02d431e92
TuanITNe <21130235@st.hcmuaf.edu.vn>
2024-07-11 11:54:16 +0700

    add translate

--- src/component/Shop/Filter/ProductFilter.tsx
+++ src/component/Shop/Filter/ProductFilter.tsx
@@ -1,25 +1,43 @@
 import React from "react";
+import "./ProductFilter.scss"
 import FilterByCategory from "./FilterByCategory";
+import {useTranslation} from "react-i18next";
 
 interface Filter {
 
 }
 
 const ProductFilter = (filter?: Filter) => {
-    return (<div className="sidebar">
-        <h3>Sản phẩm hiện có ()</h3>
-        <div className="category-buttons">
-        </div>
-        <h4>Giới tính</h4>
-        <label><input type="checkbox"/> Nam</label>
-        <label><input type="checkbox"/> Nữ</label>
-        <label><input type="checkbox"/> Unisex</label>
-        <h4>Đặc tính</h4>
-        <label><input type="checkbox"/> Sẵn hàng</label>
-        <label><input type="checkbox"/> Pre order</label>
-        <h4>Khoảng giá</h4>
-        <label><input type="checkbox"/> 550.000 - 1.200.000đ</label>
-    </div>)
+
+    const {t} = useTranslation('productfitler')
+
+    return (
+        <div className="sidebar">
+            <h3 className={"sidebar-h3"}>{t('product filter.existing products')} ()</h3>
+                <div className="category-buttons">
+                        <label className={"check"}><input type="checkbox"/>{t('product filter.male')}</label>
+                </div>
+                <h4 className={"sidebar-h4"}>{t('product filter.gender')}</h4>
+                <label className={"check"}><input type="checkbox"/>{t('product filter.male')}</label>
+            <label className={"check"}><input type="checkbox"/>{t('product filter.female')}</label>
+            <label className={"check"}><input type="checkbox"/>{t('product filter.Unisex')}</label>
+            <h4 className={"sidebar-h4"}>{t('product filter.attributers')}</h4>
+            <label className={"check"}><input type="checkbox"/>{t('product filter.in stock')}</label>
+            <label className={"check"}><input type="checkbox"/>{t('product filter.Pre order')}</label>
+            <h4 className={"sidebar-h4"}>{t('product filter.Price Range')}</h4>
+            <label className={"check"}><input type="checkbox"/> 550.000đ - 1.200.000đ</label>
+            <label className={"check"}><input type="checkbox"/> 1.200.000đ - 2.000.000đ</label>
+            <label className={"check"}><input type="checkbox"/> 2.000.000đ - 2.500.000đ</label>
+            <h4 className={"sidebar-h4"}>{t('product filter.Brand')}</h4>
+            <label className={"check"}><input type="checkbox"/>{t('product filter.Nike')}</label>
+            <label className={"check"}><input type="checkbox"/>{t('product filter.Nike')}</label>
+            <label className={"check"}><input type="checkbox"/>{t('product filter.Nike')}</label>
+            <label className={"check"}><input type="checkbox"/>{t('product filter.Nike')}</label>
+            <label className={"check"}><input type="checkbox"/>{t('product filter.Nike')}</label>
+            <label className={"check"}><input type="checkbox"/>{t('product filter.Nike')}</label>
+            <label className={"check"}><input type="checkbox"/>{t('product filter.Nike')}</label>
+            <label className={"check"}><input type="checkbox"/>{t('product filter.Nike')}</label>
+        </div>)
 }
 
 export default ProductFilter;
